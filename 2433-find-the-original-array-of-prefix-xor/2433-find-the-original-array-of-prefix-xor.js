d685baf9bb46437b741b0310b416560cbab26fab
diningm1213 <154329369+diningm1213@users.noreply.github.com>
2023-12-21 23:48:58 +0900

    Time: 149 ms (93.39%), Space: 71.9 MB (47.89%) - LeetHub

--- 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.js
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.js
@@ -6,7 +6,7 @@ var findArray = function(pref) {
     const arr = [pref[0]];
     
     for (let i = 1; i < pref.length; i++) {
-        arr[i] = pref[i] ^ pref[i - 1];
+        arr.push(pref[i] ^ pref[i - 1]);
     }
 
     return arr;
