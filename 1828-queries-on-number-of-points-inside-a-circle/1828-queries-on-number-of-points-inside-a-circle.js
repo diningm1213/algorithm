7ce40c5825ff013ccd2cc1cf5fbf58e3c1a2c30b
diningm1213 <154329369+diningm1213@users.noreply.github.com>
2023-12-31 00:00:14 +0900

    Time: 151 ms (7.21%), Space: 45.9 MB (39.64%) - LeetHub

--- 1828-queries-on-number-of-points-inside-a-circle/1828-queries-on-number-of-points-inside-a-circle.js
+++ 1828-queries-on-number-of-points-inside-a-circle/1828-queries-on-number-of-points-inside-a-circle.js
@@ -7,8 +7,8 @@ var countPoints = function(points, queries) {
     return queries.map(([x1, y1, r]) => {
         let insideCircleCount = 0;
         points.forEach(([x2, y2]) => {
-            const euclideanDist = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-            if (euclideanDist <= r) {
+            const euclideanDist = (x2 - x1) ** 2 + (y2 - y1) ** 2;
+            if (euclideanDist <= r ** 2) {
                 insideCircleCount++;
             }
         })
