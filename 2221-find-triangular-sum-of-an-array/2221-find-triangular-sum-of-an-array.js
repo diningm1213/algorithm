c03091391867179f50cba6d0418196e43c8bc316
diningm1213 <154329369+diningm1213@users.noreply.github.com>
2024-02-04 12:21:33 +0900

    Time: 304 ms (13.39%), Space: 51.3 MB (39.37%) - LeetHub

---
+++ 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var triangularSum = function(nums) {
+    while(nums.length !== 1) {
+        for (let i = 0; i < nums.length; i++) {
+            nums[i] = (nums[i] + nums[i + 1]) % 10;
+        }
+        nums.pop();
+    }
+
+    return nums[0];
+};
