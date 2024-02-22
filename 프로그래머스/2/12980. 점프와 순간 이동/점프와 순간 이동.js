bc27dd5c717291bd73d61d8b2e974a611cf644d4
diningm1213 <154329369+diningm1213@users.noreply.github.com>
2024-02-22 15:08:13 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,14 +1,4 @@
 function solution(n)
 {
-    let count = 0;
-    while (n > 0) {
-        if (n % 2 === 0) {
-            n /= 2;
-        } else {
-            n -= 1;
-            count++;
-        }
-    }
-    
-    return count;
+    return [...n.toString(2)].reduce((acc, cur) => +acc + +cur, 0);
 }
