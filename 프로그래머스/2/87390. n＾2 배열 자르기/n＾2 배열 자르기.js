2d05db4cbebf311437097953094242a4ab00e986
diningm1213 <154329369+diningm1213@users.noreply.github.com>
2024-02-24 12:39:14 +0900

    [level 2] Title: n^2 배열 자르기, Time: 6.29 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -2,10 +2,7 @@ function solution(n, left, right) {
     const arr = [];
     
     for (left; left <= right; left++) {
-        const quotient = Number.parseInt(left / n);
-        const remainder = left % n;
-        
-        arr.push(Math.max(quotient, remainder) + 1);
+        arr.push(Math.max(parseInt(left / n), left % n) + 1);
     }
     
     return arr;
