35de554f785d3df2aa00516b6f9524f17ff0b4af
diningm1213 <154329369+diningm1213@users.noreply.github.com>
2024-02-28 13:26:55 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,21 +1,9 @@
 function solution(n) {
-    if (n === 1) {
-        return 1;
+    const dp = [1, 1];
+    
+    for (let i = 2; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567; 
     }
     
-    if (n === 2) {
-        return 2;
-    }
-    
-    let n1 = 1;
-    let n2 = 2;
-    
-    while (n > 2) {
-        let temp = (n1 + n2) % 1234567;
-        n1 = n2;
-        n2 = temp;
-        n--;
-    }
-    
-    return n2; 
+    return dp[n];
 }
