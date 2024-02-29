16afaabf0d52bcec0cd7ad897eed4fecdf74bbf3
diningm1213 <154329369+diningm1213@users.noreply.github.com>
2024-02-29 15:24:06 +0900

    [level 2] Title: 전화번호 목록, Time: 106.47 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,18 +1,7 @@
 function solution(phone_book) {
-    const phoneBookSet = new Set();
-    const phoneLengthSet = new Set();
-    
-    phone_book.sort((a, b) => a.length - b.length);
-    for (let i = 0; i < phone_book.length; i++) {
-        const phone = phone_book[i];
-        for (const len of phoneLengthSet) {
-            if (phoneBookSet.has(phone.substring(0, len))) {
-                return false;
-            }
-        }
-        phoneBookSet.add(phone);
-        phoneLengthSet.add(phone.length);
-    }
-    
-    return true;
+    return !phone_book.sort().some((_, i) => {
+        if (i === phone_book.length - 1) return false;
+        
+        return phone_book[i + 1].startsWith(phone_book[i]);
+    });
 }
