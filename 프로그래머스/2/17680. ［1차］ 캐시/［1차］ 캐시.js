4e0dbea2e0aba6c72f92ebfdc1b7e8c8d6c81ae7
diningm1213 <154329369+diningm1213@users.noreply.github.com>
2024-03-03 18:12:40 +0900

    [level 2] Title: [1차] 캐시, Time: 1.07 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -5,15 +5,16 @@ function solution(cacheSize, cities) {
     cities.forEach(_city => {
         const city = _city.toLowerCase();
         const findIndex = cache.findIndex(c => c === city);
+        
+        cache.push(city);
+        
         if (findIndex === -1) {
-            cache.push(city);
             if (cache.length > cacheSize) {
                 cache.shift();
             }
             runTime += 5;
         } else {
             cache.splice(findIndex, 1);
-            cache.push(city);
             runTime++;
         }
     });
