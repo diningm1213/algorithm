712b7661102ec15d4f051074c6060c71d8e62c47
diningm1213 <154329369+diningm1213@users.noreply.github.com>
2024-03-24 21:03:05 +0900

    [level 1] Title: 삼총사, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -0,0 +1,16 @@
+function solution(number) {
+    let ans = 0;
+    const len = number.length;
+    
+    for (let i = 0; i < len - 2; i++) {
+        for (let j = i + 1; j < len - 1; j++) {
+            for (let k = j + 1; k < len; k++) {
+                if (number[i] + number[j] + number[k] === 0) {
+                    ans++;
+                }
+            }
+        }
+    }
+    
+    return ans;
+}
