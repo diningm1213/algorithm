3080b1ea2881b9070e18867716b2259858ee8a1e
diningm1213 <154329369+diningm1213@users.noreply.github.com>
2024-03-26 19:35:08 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,19 +1,19 @@
 function solution(s) {
     const numbers = [
-        [0, 'zero'],
-        [1,	'one'],
-        [2,	'two'],
-        [3,	'three'],
-        [4,	'four'],
-        [5,	'five'],
-        [6,	'six'],
-        [7,	'seven'],
-        [8,	'eight'],
-        [9,	'nine'],
+        'zero',
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine',
     ];
     
-    numbers.forEach(([n, word]) => {
-        s = s.replaceAll(word, n);
+    numbers.forEach((n, i) => {
+        s = s.replaceAll(n, i);
     });
 
     return +s;
