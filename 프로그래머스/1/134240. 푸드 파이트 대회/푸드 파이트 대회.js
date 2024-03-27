472cd004fc0a4660537ca6ae9a76fa1d7f50a550
diningm1213 <154329369+diningm1213@users.noreply.github.com>
2024-03-27 22:32:47 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,14 +1,4 @@
 function solution(food) {
-    let foods = [];
-    food.forEach((f, i) => {
-        if (i === 0) {
-            return;
-        }
-        
-        for (let j = 2; j <= f; j += 2) {
-            foods.push(i);
-        }
-    });
-    
-    return foods.join('') + '0' + foods.reverse().join('');
+    var answer = '';
+    return answer;
 }
