c9d63fb07de6e532ee49b13546b58a83daa9d609
diningm1213 <154329369+diningm1213@users.noreply.github.com>
2024-05-05 23:47:53 +0900

    [level 0] Title: 문자열 밀기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -0,0 +1,11 @@
+function solution(A, B) {
+    for (let i = 0; i < A.length; i++) {
+        if (A === B) {
+            return i;
+        } 
+        
+        A = A.at(-1) + A.slice(0, A.length - 1);
+    }
+    
+    return -1;
+}
