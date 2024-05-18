8ef7923386d3c507bf66f9cbde2bfdb010f43bec
diningm1213 <154329369+diningm1213@users.noreply.github.com>
2024-05-18 21:20:44 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -0,0 +1,9 @@
+function solution(my_str, n) {
+    const ans = [];
+    
+    for (let i = 0; i < my_str.length; i += n) {
+        ans.push(my_str.slice(i, i + n));
+    }
+    
+    return ans;
+}
