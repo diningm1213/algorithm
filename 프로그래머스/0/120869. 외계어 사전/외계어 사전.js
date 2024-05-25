8cc29370f94c0a9af53e835ebf44f0f3426eaf87
diningm1213 <154329369+diningm1213@users.noreply.github.com>
2024-05-25 22:04:35 +0900

    [level 0] Title: 외계어 사전, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
@@ -1,10 +1,5 @@
 function solution(spell, dic) {
     spell = spell.sort().join('');
-    for (let i = 0; i < dic.length; i++) {
-        if (spell === dic[i].split('').sort().join('')) {
-            return 1;
-        }
-    }
     
-    return 2;
+    return dic.some(d => [...d].sort().join('') === spell) ? 1 : 2;
 }
