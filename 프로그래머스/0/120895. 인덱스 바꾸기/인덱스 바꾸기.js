95bf6c20a08af17afae9591c9cc3c7d7335cc125
diningm1213 <154329369+diningm1213@users.noreply.github.com>
2024-06-12 08:27:39 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,7 +1,5 @@
 function solution(my_string, num1, num2) {
     my_string = [...my_string];
-    const temp = my_string[num1];
-    my_string[num1] = my_string[num2];
-    my_string[num2] = temp;
+    [my_string[num1], my_string[num2]] = [my_string[num2], my_string[num1]]
     return my_string.join('');
 }
