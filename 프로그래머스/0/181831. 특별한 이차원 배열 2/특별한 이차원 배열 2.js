9e3dd4ca8daee5e049c233e275cebaa013a7b40a
diningm1213 <154329369+diningm1213@users.noreply.github.com>
2024-06-17 22:16:04 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -1,12 +1,3 @@
 function solution(arr) {
-    const n = arr[0].length
-    for (let y = 0; y < n; y++) {
-        for (let x = 0; x < n; x++) {
-            if (arr[y][x] !== arr[x][y]) {
-                return 0;
-            }
-        }
-    }
-    
-    return 1;
+    return arr.every((r, i) => r.every((c, j) => arr[i][j] === arr[j][i])) ? 1 : 0;
 }
