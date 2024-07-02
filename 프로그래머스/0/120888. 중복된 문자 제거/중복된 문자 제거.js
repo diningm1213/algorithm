9f2aeb31a028f4216b508b2590f7f04bbd1769fe
diningm1213 <154329369+diningm1213@users.noreply.github.com>
2024-07-02 13:32:11 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,11 +1,3 @@
 function solution(my_string) {
-    const set = new Set();
-    
-    return [...my_string].filter(c => {
-        if (!set.has(c)) {
-            set.add(c);
-            return true;
-        }
-        return false;
-    }).join('');
+    return [...new Set(my_string)].join('');
 }
