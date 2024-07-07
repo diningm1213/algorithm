948f81a1466c45f74137e37529345b436a307199
diningm1213 <154329369+diningm1213@users.noreply.github.com>
2024-07-07 13:38:42 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
@@ -1,12 +1,4 @@
 function solution(myString, pat) {
-    let count = 0;
-    let patLen = pat.length;
-    
-    for (let i = 0; i <= myString.length - patLen; i++) {
-        if (myString.substring(i, i + patLen) === pat) {
-            count++;
-        }
-    }
-    
-    return count;
+    const reg = new RegExp(`(?=${pat})`, "g")
+    return myString.match(reg)?.length || 0;
 }
