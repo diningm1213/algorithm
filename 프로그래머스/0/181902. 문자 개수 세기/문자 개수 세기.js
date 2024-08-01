28462cbf722fd69e8848495c8ff0beaa71e183f8
diningm1213 <154329369+diningm1213@users.noreply.github.com>
2024-08-01 09:33:41 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.90 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
@@ -0,0 +1,18 @@
+function solution(my_string) {
+    const ALPHABET_LEN = 26;
+    const ans = Array(ALPHABET_LEN * 2).fill(0);
+    
+    
+    [...my_string].forEach(c => {
+        let index = 0;
+        if (c.match(/[A-Z]/)) {
+            index = c.charCodeAt() - 'A'.charCodeAt();
+            ans[index]++;
+        } else if (c.match(/[a-z]/)) {
+            index = c.charCodeAt() - 'a'.charCodeAt();
+            ans[index + ALPHABET_LEN]++;
+        }
+    });
+    
+    return ans;
+}
