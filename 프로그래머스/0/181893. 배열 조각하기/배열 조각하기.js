54e42323e068ad8655e84a2ca5246493a84c9842
diningm1213 <154329369+diningm1213@users.noreply.github.com>
2024-08-08 23:50:16 +0900

    [level 0] Title: 배열 조각하기, Time: 1.90 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
@@ -0,0 +1,11 @@
+function solution(arr, query) { 
+    query.forEach((v, i) => {
+        if (i % 2 === 0) {
+            arr = arr.slice(0, v + 1);
+        } else {
+            arr = arr.slice(v);
+        }
+    });
+    
+    return arr;
+}
