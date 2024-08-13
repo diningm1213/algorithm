3fdc6f10b3c6dbe9fdcf2206e0d55edcc4fd414e
diningm1213 <154329369+diningm1213@users.noreply.github.com>
2024-08-13 23:09:06 +0900

    [level 0] Title: 특이한 정렬, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
@@ -1,12 +1,4 @@
 function solution(numlist, n) {
-    return numlist.sort((a, b) => {
-        const na = Math.abs(n - a);
-        const nb = Math.abs(n - b);
-        
-        if (na === nb) {
-            return b - a;
-        }
-        
-        return na - nb;
-    });
+    return numlist.sort((a, b) => 
+        Math.abs(n - a) - Math.abs(n - b) || b - a);
 }
