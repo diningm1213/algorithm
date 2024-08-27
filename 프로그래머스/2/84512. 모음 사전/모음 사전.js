0e1b497cef571e116a324b5fca9566c6ccf501fa
diningm1213 <154329369+diningm1213@users.noreply.github.com>
2024-08-27 14:22:58 +0900

    [level 2] Title: 모음 사전, Time: 8.53 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,24 +1,20 @@
 function solution(_word) {
     const ans = new Set();
+    const words = ['', 'A', 'E', 'I', 'O', 'U'];
     const recursion = (word, deps) => {
         if (deps === 4) {
             ans.add(word.join(''));
             return;
         }
         
-        recursion([...word, ''], deps + 1);
-        recursion([...word, 'A'], deps + 1);
-        recursion([...word, 'E'], deps + 1);
-        recursion([...word, 'I'], deps + 1);
-        recursion([...word, 'O'], deps + 1);
-        recursion([...word, 'U'], deps + 1);
+        words.forEach(w => {
+            recursion([...word, w], deps + 1);
+        });
     }
     
-    recursion(['A'], 0);
-    recursion(['E'], 0);
-    recursion(['I'], 0);
-    recursion(['O'], 0);
-    recursion(['U'], 0);
+    words.forEach(w => {
+        recursion([w], 0);
+    })
     
-    return [...ans].sort().indexOf(_word) + 1;
+    return [...ans].sort().indexOf(_word);
 }
