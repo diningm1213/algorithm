20d5a97a5dce4c59ffa27fb1b325e359ac8c93b5
diningm1213 <154329369+diningm1213@users.noreply.github.com>
2024-09-05 21:18:10 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 19.84 ms, Memory: 40.3 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -4,18 +4,14 @@ function solution(files) {
         const head = file.substring(0, match.index);
         const number = match[0];
         const tail = file.substring(match.index + number.length)
+        
         return [head, number, tail];
     }).sort(([aHead, aNumber, aTail], [bHead, bNumber, bTail]) => {
         aHead = aHead.toLowerCase();
         bHead = bHead.toLowerCase();
-        aNumber = Number(aNumber);
-        bNumber = Number(bNumber);
-        aTail = aTail.toLowerCase();
-        bTail = bTail.toLowerCase();
         
         return aHead === bHead ? 
             aNumber === bNumber ? 0 : aNumber - bNumber :
             aHead.localeCompare(bHead);
-        
     }).map(file => file.join(''));
 }
